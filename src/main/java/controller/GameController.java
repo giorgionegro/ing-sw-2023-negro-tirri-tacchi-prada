bd21e6cbdcfe03bc9c84a09084f6ca738688ce34
Enrico <enricotirri@gmail.com>
2023-04-04 16:04:12 +0200

    Collapsed controllers into GameController

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,7 +1,13 @@
 package controller;
 
-public class GameController {
-    public void signalEndOfTurn(){
+import model.PlayerMove;
+import model.abstractModel.Message;
 
-    }
+public class GameController {
+
+    public void doPlayerMove(PlayerMove move){}
+
+    public void sendMessage(Message newMessage){}
+
+    //TODO funzioni di evaluate
 }
