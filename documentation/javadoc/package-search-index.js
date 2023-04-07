10759a7b6bc548fba3e0f38428b7164f873fbf28
Giorgio <giorgionegro@protonmail.com>
2023-04-07 18:04:43 +0200

    fallback

--- documentation/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.abstractModel"},{"l":"model.exceptions"},{"l":"model.goalEvaluators"}];updateSearchResults();
