b3783d228501b658e32db3baaf0b6b6273bdd379
Giorgio <giorgionegro@protonmail.com>
2023-04-12 11:46:18 +0200

    added some constructors and javadocs

--- src/main/java/model/PickedTile.java
+++ src/main/java/model/PickedTile.java
@@ -1,12 +1,45 @@
 package model;
 
+/**
+ * Class that represents a picked tile
+ */
 public class PickedTile {
+
+
+    /**
+     * row of the tile
+     */
+
     private int row;
+
+
+    /**
+     * column of the tile
+     */
+
     private int col;
 
+    /**
+     * Constructor
+     *
+     * @param row row of the tile
+     * @param col column of the tile
+     */
+    public PickedTile(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    /**
+     * @return row of the tile
+     */
     public int getRow() {
         return row;
     }
+
+    /**
+     * @return column of the tile
+     */
 
     public int getCol() {
         return col;
