dbae5118fcad2cc818d45c31386879ce0bbf4a3a
Enrico <enricotirri@gmail.com>
2023-04-14 17:32:50 +0200

    Removed Chat, replaced with PlayerChat
    Make Player an abstract class
    Make Player Observable
    
    Implemented StandardPlayer
    Implemented StandardPlayerChat
    
    Completed javadoc for Player, PlayerChat, StandardPlayer, StandardPlayerChat

---
+++ src/main/java/model/StandardPlayerChat.java
@@ -0,0 +1,45 @@
+package model;
+
+import model.abstractModel.PlayerChat;
+import model.abstractModel.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is an implementation of PlayerChat.
+ * <p>
+ * It uses a List to manage messages
+ */
+public class StandardPlayerChat extends PlayerChat {
+
+    /**
+     * messages sent to this player chat (list of messages)
+     */
+    private final List<Message> messages;
+
+    /**
+     * Constructor of an empty player chat
+     */
+    public StandardPlayerChat(){
+        this.messages = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param newMessage new message that has to be added to chat
+     */
+    @Override
+    public void addMessage(Message newMessage) {
+        this.messages.add(newMessage);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return a copy of {@link #messages}
+     */
+    @Override
+    public List<Message> getMessages() {
+        return this.messages;
+    }
+}
