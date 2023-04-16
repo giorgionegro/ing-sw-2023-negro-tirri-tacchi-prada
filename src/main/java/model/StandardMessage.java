b3027e412b29eb785574f80590c14bee5f5137bd
Enrico <enricotirri@gmail.com>
2023-04-16 18:27:30 +0200

    Removed MessageSubject enum, replaced by string
    Completed javadoc of StandardMessage

--- src/main/java/model/StandardMessage.java
+++ src/main/java/model/StandardMessage.java
@@ -7,35 +7,71 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/**
+ * This class is an implementation of {@link Message}
+ */
 public class StandardMessage implements Message {
 
+    /**
+     * The id of the message sender
+     */
     private final String idSender;
-    private final MessageSubject subject;
+    /**
+     * The message subject
+     */
+    private final String subject;
+    /**
+     * The message text content
+     */
     private final String text;
+    /**
+     * The {@link String} representation of the timestamp of instance creation
+     */
     private final String timestamp;
 
-    public StandardMessage(String idSender, MessageSubject subject, String text){
+    /**
+     * Construct an instance with given sender id, subject and text, timestamp is set on localtime at instance creation
+     * @param idSender the id of the sender
+     * @param subject the subject of the message
+     * @param text an {@link String} representation of message text content
+     */
+    public StandardMessage(String idSender, String subject, String text){
         this.idSender = idSender;
         this.subject = subject;
         this.text = text;
         this.timestamp = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withLocale(Locale.getDefault()).withZone(ZoneId.systemDefault()).format(Instant.now());
     }
 
-
+    /**
+     * {@inheritDoc}
+     * @return {@link #idSender}
+     */
     @Override
     public String getSender() {
         return idSender;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@link #subject}
+     */
     @Override
-    public MessageSubject getSubject() {
+    public String getSubject() {
         return subject;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@link #text}
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@link #timestamp}
+     */
     @Override
     public String getTimestamp() {
         return timestamp;
@@ -44,7 +80,7 @@ public class StandardMessage implements Message {
     @Override
     public String toString(){
         return "Sender: "+idSender+"\n"
-                +"Receiver: "+subject.getSubjectId()+"\n"
+                +"Receiver: "+subject+"\n"
                 +"Text: "+text+"\n"
                 +"TimeStamp: "+timestamp+"\n";
     }
--- src/main/java/model/abstractModel/Message.java
+++ src/main/java/model/abstractModel/Message.java
@@ -5,46 +5,18 @@ package model.abstractModel;
  */
 public interface Message {
     /**
-     * This enumeration contains the subject to which the message has been sent
-     */
-    enum MessageSubject {
-        /**
-         * This subject means that the message has been sent to everybody is playing the same game
-         */
-        BROADCAST("");
-        /**
-         * The id of the player to which the message has been sent
-         */
-        private final String subjectId;
-
-        /**
-         * Construct a MessageSubject with the given subjectId
-         * @param subjectId the id of the subject of the message
-         */
-        MessageSubject(String subjectId){
-            this.subjectId = subjectId;
-        }
-
-        /**
-         * This method returns the id of the subject of the message
-         * @return the id of the subject of the message
-         */
-        public String getSubjectId() {
-            return subjectId;
-        }
-    }
-
-    /**
      * This method returns the id of the sender of the message
      * @return the id of the sender of the message
      */
     String getSender();
 
     /**
-     * This method returns the MessageSubject of the message
+     * This method returns the id of the subject of the message
+     * <p>
+     * If {@code subject.isEmpty()==true} then the message is supposed to be sent broadcast
      * @return the MessageSubject of the message
      */
-    MessageSubject getSubject();
+    String getSubject();
 
     /**
      * This method returns the text-content of the message
