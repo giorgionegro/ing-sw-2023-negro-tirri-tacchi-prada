7063112dafcfefdcccc9fa590ada0f1080b18a8b
Enrico <enricotirri@gmail.com>
2023-04-19 10:28:17 +0200

    Add shelf object

---
+++ src/main/java/model/StandardShelf.java
@@ -0,0 +1,26 @@
+package model;
+
+import model.abstractModel.Shelf;
+
+import java.util.Arrays;
+
+public class StandardShelf extends Shelf {
+
+    Tile[][] shelf;
+
+    public StandardShelf(){
+        this.shelf = new Tile[6][5];
+    }
+
+    @Override
+    public Tile[][] getShelf() {
+        return Arrays.stream(shelf).map(Tile[]::clone).toArray(Tile[][]::new);
+    }
+
+    @Override
+    public void setShelf(Tile[][] modifiedShelf) {
+        this.shelf = Arrays.stream(modifiedShelf).map(Tile[]::clone).toArray(Tile[][]::new);
+        setChanged();
+        notifyObservers(ShelfEvent.SHELF_MODIFIED);
+    }
+}
