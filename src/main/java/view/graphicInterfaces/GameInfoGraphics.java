a43cc66ee4cf6c6eadab7478f7850bf8b3ba3f81
Rebecca <rebeccamaria.tacchi@mail.polimi.it>
2023-06-23 18:52:06 +0200

    Added Javadoc at ModelView

--- src/main/java/view/graphicInterfaces/GameInfoGraphics.java
+++ src/main/java/view/graphicInterfaces/GameInfoGraphics.java
@@ -3,7 +3,16 @@ package view.graphicInterfaces;
 import model.abstractModel.Game;
 
 import java.util.Map;
-
+/**
+ * This interface represents the graphics for the current match.
+ * It provides a method to update the information of the match when the users are playing the game.
+ */
 public interface GameInfoGraphics {
+    /**This method updates the information of the match with the specified parameters:
+     @param status status of the game
+     @param playerOnTurn id of the player on turn
+     @param isLastTurn true if the game is in the last round of turns
+     @param pointsValues points amount of each player
+     */
     void updateGameInfoGraphics(Game.GameStatus status, String playerOnTurn, boolean isLastTurn, Map<String,Integer> pointsValues);
 }
--- src/main/java/view/graphicInterfaces/PlayerInfoGraphics.java
+++ src/main/java/view/graphicInterfaces/PlayerInfoGraphics.java
@@ -1,7 +1,20 @@
 package view.graphicInterfaces;
 import model.Token;
 import java.util.Map;
+
+
+/**
+ * This interface represents the graphics for a common goal completed and an error reported.
+ * It provides two methods to update the information of the player's info during the game.
+ */
 public interface PlayerInfoGraphics {
+    /**This method updates the information of the player achieving a common goal with the specified parameter:
+     * @param achievedCommonGoals map of achieved common goals with earned token
+     */
     void updateAchievedCommonGoals(Map<String,Token> achievedCommonGoals);
+
+    /**This method updates the information of an error reported during the match with the specified parameter:
+     * @param reportedError message of en error encountered during gameplay
+     */
     void updateErrorState(String reportedError);
 }
